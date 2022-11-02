c04e4d96b8c0f2631c284ef1df1eea1032471e6c
Oleksandra-Sapielnikova <73056445+Oleksandra-Sapielnikova@users.noreply.github.com>
2022-11-02 22:18:16 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Oleksandra Sapielnikova and is {""}
-        <a
-          href="https://github.com/Oleksandra-Sapielnikova/react-weatherapp-project-01.11.22"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Oleksandra Sapielnikova and is {""}
+          <a
+            href="https://github.com/Oleksandra-Sapielnikova/react-weatherapp-project-01.11.22"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
