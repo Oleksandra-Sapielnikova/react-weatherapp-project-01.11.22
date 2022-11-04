1daf0ee006079ae2b26545024217dca7f4e7a6a6
Oleksandra-Sapielnikova <73056445+Oleksandra-Sapielnikova@users.noreply.github.com>
2022-11-04 22:52:56 +0100

    added link to my instagram and netlify hosting of the app

--- src/App.js
+++ src/App.js
@@ -5,15 +5,32 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Tokyo"/>
+        <Weather defaultCity="Tokyo" />
         <footer>
-          This project was coded by Oleksandra Sapielnikova and is {""}
+          This project was coded by{" "}
+          <a
+            href="https://www.instagram.com/oleksasapielnikova/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Oleksandra Sapielnikova
+          </a>
+          , it is {""}
           <a
             href="https://github.com/Oleksandra-Sapielnikova/react-weatherapp-project-01.11.22"
             target="_blank"
             rel="noreferrer"
           >
             open-sourced on GitHub
+          </a>{" "}
+          and hosten on{" "}
+          <a
+            href="https://frabjous-swan-515076.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
           </a>
         </footer>
       </div>
