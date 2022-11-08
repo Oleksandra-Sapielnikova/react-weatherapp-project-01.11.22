6c850ecdc8c268d5b66326f6548dbd693794ef4a
Oleksandra-Sapielnikova <73056445+Oleksandra-Sapielnikova@users.noreply.github.com>
2022-11-08 12:55:29 +0100

    made 1 day forecast feature

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="rgb(32, 33, 36)"
-      size={58}
+      size={props.size}
       animate={true}
     />
   );
