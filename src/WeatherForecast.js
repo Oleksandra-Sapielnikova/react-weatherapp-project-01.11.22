62af211e4803c1900acd2deaf0ed1fa7929fb518
Oleksandra-Sapielnikova <73056445+Oleksandra-Sapielnikova@users.noreply.github.com>
2022-11-09 23:24:33 +0100

    made a forecast api call

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,21 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  console.log(props);
+
+  let apiKey = `0364b407f3accb49cafbf8oc82bf5f2t`;
+  let longitude = props.coordinates.longitude;
+  let latitude = props.coordinates.latitude;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
